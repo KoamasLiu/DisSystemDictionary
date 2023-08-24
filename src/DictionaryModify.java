666d57496a2f186e46f482c1d14575bbf2b7e9e2
KoamasLiu <lhyperaion@gmail.com>
2023-08-24 23:40:33 +1000

    new change

--- src/DictionaryModify.java
+++ src/DictionaryModify.java
@@ -5,6 +5,7 @@ import java.util.Map;
 public class DictionaryModify {
     private final String filePath;
     private final Map<String, String> entries;
+    private boolean Dictionary_Occupancy = false;
 
     public DictionaryModify(String filePath) throws IOException {
         this.filePath = filePath;
@@ -24,42 +25,60 @@ public class DictionaryModify {
         }
     }
 
-    public String query(String word) {
-        return entries.get(word);
-    }
-
-    public String add(String word, String definition) throws IOException {
-        if (entries.containsKey(word)) {
-            return "Duplicate";
-        }
-        entries.put(word, definition);
-        saveToFile();
-        return "Success";
-    }
-
-    public String remove(String word) throws IOException {
-        if (entries.remove(word) != null) {
-            saveToFile();
-            return "Success";
-        }
-        return "Not found";
-    }
-
-    public String update(String word, String definition) throws IOException {
-        if (!entries.containsKey(word)) {
-            return "Not found";
-        }
-        entries.put(word, definition);
-        saveToFile();
-        return "Success";
-    }
-
-    private void saveToFile() throws IOException {
+    synchronized private void saveToFile() throws IOException {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath))) {
             for (Map.Entry<String, String> entry : entries.entrySet()) {
                 writer.write(entry.getKey() + ":" + entry.getValue());
                 writer.newLine();
             }
         }
+    }
+
+    public String query(String word) throws InterruptedException {
+        while (Dictionary_Occupancy){
+            wait();
+        }
+        return entries.get(word);
+    }
+
+    synchronized public String add(String word, String definition) throws IOException, InterruptedException {
+        while (Dictionary_Occupancy){
+            wait();
+        }
+        if (entries.containsKey(word)) {
+            return "Duplicate";
+        }
+        entries.put(word, definition);
+        saveToFile();
+        Dictionary_Occupancy = false;
+        notifyAll();
+        return "Success";
+    }
+
+    synchronized public String remove(String word) throws IOException, InterruptedException {
+        while (Dictionary_Occupancy){
+            wait();
+        }
+        if (entries.remove(word) != null) {
+            saveToFile();
+            return "Success";
+        }
+        Dictionary_Occupancy = false;
+        notifyAll();
+        return "Not found";
+    }
+
+    synchronized public String update(String word, String definition) throws IOException, InterruptedException {
+        while (Dictionary_Occupancy){
+            wait();
+        }
+        if (!entries.containsKey(word)) {
+            return "Not found";
+        }
+        entries.put(word, definition);
+        saveToFile();
+        Dictionary_Occupancy = false;
+        notifyAll();
+        return "Success";
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,66 @@
 import java.io.IOException;
-import java.util.Dictionary;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+
+import javax.net.ServerSocketFactory;
 
 public class Main {
+    private static int port = 6666;
+    private static int counter = 0;
     public static void main(String[] args) {
+        ServerSocketFactory factory = ServerSocketFactory.getDefault();
+
+        try(ServerSocket server = factory.createServerSocket(port)) {
+            System.out.println("Waiting for client connection-");
+
+            while(true) {
+                Socket client = server.accept();
+                counter++;
+                System.out.println("Client "+counter+": Applying for connection!");
+
+                Thread t = new Thread(() -> serveClient(client));
+                t.start();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         try {
             DictionaryModify dict = new DictionaryModify("Dictionary.txt");
             System.out.println(dict.query("apple"));   // Outputs: A fruit that is usually red or green.
             System.out.println(dict.add("banana", "A yellow fruit."));  // Outputs: Success
             System.out.println(dict.remove("book"));   // Outputs: Success
             System.out.println(dict.update("car", "A four-wheeled vehicle."));   // Outputs: Success
-        } catch (IOException e) {
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+    private static void serveClient(Socket client) {
+        try(Socket clientSocket = client) {
+
+
+
+
+
+            DataInputStream input = new DataInputStream(clientSocket.getInputStream());
+            // Output Stream
+            DataOutputStream output = new DataOutputStream(clientSocket.getOutputStream());
+
+            System.out.println("CLIENT: "+input.readUTF());
+
+            output.writeUTF("Server: Hi Client "+counter+" !!!");
+        }
+        catch (IOException e)
+        {
             e.printStackTrace();
         }
     }
+
+
 }
